b0f9c5561bbbe5786f40da62d982ed5774233586
usertiagosilva <tiago_ferreira.s@hotmail.com>
2024-06-04 19:21:37 -0300

    commit CSS e JS

--- tabuada-js/js/scripts.js
+++ tabuada-js/js/scripts.js
@@ -1,1 +1,26 @@
-console.log("Teste");
+// Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+
+    // Limpar resultados
+    multiplicationTable.innerHTML = "";
+};
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+
+   const multiplicationNumber = +numberInput.value;
+
+   const multiplicatorNumber = +multiplicationInput.value;
+
+    //  Se nao digitar valores retorne
+   if (!multiplicationNumber || !multiplicatorNumber) return;
+
+   createTable(multiplicationNumber, multiplicatorNumber);
+});
