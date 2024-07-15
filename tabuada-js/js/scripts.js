98838326447bf395c1fcc2c8c9fbb69e424f1e0f
usertiagosilva <tiago_ferreira.s@hotmail.com>
2024-07-15 12:03:53 -0300

    commit tabuada dinâmica

--- tabuada-js/js/scripts.js
+++ tabuada-js/js/scripts.js
@@ -3,12 +3,37 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 const multiplicationTable = document.querySelector("#multiplication-operations");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
 
 // Funções
+// Criar tabuada
 const createTable = (number, multiplicatorNumber) => {
 
-    // Limpar resultados
+    // Limpar HTML
     multiplicationTable.innerHTML = "";
+
+    // Loop para percorrer tabuada
+    for(i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        // Template string
+        const template = ` <div class= "row">
+                   <div class="operation"> ${number} x ${i} = </div>
+                   <div class="result"> ${result} </div>  
+            </div>`;
+
+        //  Transferir string para o HTML
+        const parser = new DOMParser(); 
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+    }
+
+    // Exibir numero da tabuada
+    multiplicationTitle.innerText = number;
 };
 
 // Eventos
@@ -19,7 +44,7 @@ multiplicationForm.addEventListener("submit", (e) => {
 
    const multiplicatorNumber = +multiplicationInput.value;
 
-    //  Se nao digitar valores retorne
+    //  Validação : Se nao digitar valores retorne
    if (!multiplicationNumber || !multiplicatorNumber) return;
 
    createTable(multiplicationNumber, multiplicatorNumber);
